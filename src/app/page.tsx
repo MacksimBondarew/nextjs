06f48a734881a066ae03093cfbe9e8f686096004
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-05-03 20:08:01 +0200

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import course from './course.json';
+import Lesson from "./lesson";
 
-export default function Home() {
+async function getAllCourses(): Promise<typeof course> {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(course), 1000)
+  })
+}
+
+export default async function Home() {
+  const courses = await getAllCourses();
   return (
     <main className={styles.main}>
       <ul>
-      <li>text</li>
-      {course.lessons.map((lesson) => {
+      {courses.lessons.map((lesson) => {
         return <li key={lesson.name}>
-          <h2>{lesson.title}</h2>
-          <p>{lesson.shortSummary}</p>
+<Lesson title={lesson.title} shortSummary={lesson.shortSummary} />
         </li>
       })}
       </ul>
